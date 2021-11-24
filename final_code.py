d97e46f344a8b8de105267cb60b5dfe1fa8f9e0f
19407 <80858530+19407-ehu@users.noreply.github.com>
2021-11-24 22:04:50 +0000

    final code

--- final_code.py
+++ final_code.py
@@ -1,3 +1,4 @@
+
 #Te Reo Quiz. Harley Lambert-ehu, Version 1, 25/06/2021.
 
 #Sequence 1: introduction to the game, player, and rules. Version 1, by Harley Lambert-Ehu
@@ -67,6 +68,10 @@ elif yes_or_no == "no" or yes_or_no == "n":
     print()
     print("uh-huh, I have no clue what you just said, but we move on anyways.")
 
+elif yes_or_no == "rap god" or yes_or_no == "rg":
+  print()
+  print("Uh, summa-lumma, dooma-lumma, you assumin’ I’m a human What I gotta do to get it through to you I’m superhuman? Innovative and I’m made of rubber so that anything you say is ricochetin’ off of me and it’ll glue to you and I’m devastating, more than ever demonstrating How to give a motherfuckin’ audience a feeling like it’s levitating Never fading, and I know the haters are forever waiting For the day that they can say I fell off, they’ll be celebrating ‘Cause I know the way to get ’em motivated I make elevating music, you make elevator music")
+
 else:
   print()
   print("umm...") 
@@ -99,6 +104,62 @@ elif ans1 == "b":
   print ("Your answer is...")
   time.sleep(1)
   print ("Correct! Good job on getting the first question right!")
+
+else:
+  print ("I'm sorry, thats not one of the answers. Please pick one of the three options.")
+
+print("Well the second question is preped so get ready: ")
+print("Fill in the blank. 'I'm hungry. Maybe i'll eat some _____' ")
+print()
+print("Is the blank..")
+time.sleep(0.5)
+print("A) Kaimoana")
+time.sleep(0.5)
+print()
+print("B) Moana")
+time.sleep(0.5)
+print()
+
+ans2 = input("Or C) Kai ").lower()
+
+if ans2 == "a" or ans2 == "c":
+  print ("Your answer is...")
+  time.sleep(1)
+  print ("Correct! This was actually a trick question since 'Kaimoana' translates to 'seafood' and 'kai' just translates to 'food'. So both answers wouldve been correct either way")
+  
+
+elif ans2 == "b":
+  print ("Your answer is...")
+  time.sleep(1)
+  print ("Incorrect! Just in case you did not know, the word 'moana' translates to 'ocean', whereas  ")
+
+else:
+  print ("I'm sorry, thats not one of the answers. Please pick one of the three options.")
+
+print("Anyways lets move on to the third question now, shall we? ")
+print("What is the rough translation for the word 'whakapapa'")
+print()
+print("Is it..")
+time.sleep(0.5)
+print("A) Smooth")
+time.sleep(0.5)
+print()
+print("B) Lineage")
+time.sleep(0.5)
+print()
+
+ans3 = input("Or C) Hollow ").lower()
+
+if ans3 == "a" or ans3 == "c":
+  print ("Your answer is...")
+  time.sleep(1)
+  print ("Incorrect")
+  
+
+elif ans3 == "b":
+  print ("Your answer is...")
+  time.sleep(1)
+  print ("Incorrect! Just in case you did not know, the word 'moana' translates to 'ocean', whereas ")
 
 else:
   print ("I'm sorry, thats not one of the answers. Please pick one of the three options.")
--- test.py
+++ test.py
@@ -1,21 +1,25 @@
-import time
+
+def main():
+  while True:
+
+    import time
 
 
-ans1 = input("Or C) Enemy ").lower()
+    ans1 = input("Or C) Enemy ").lower()
 
-if ans1 == "a" or ans1 == "c":
-  print ("Your answer is...")
-  time.sleep(1)
-  print ("Incorrect! The correct answer is B) Family!")
-  
+    if ans1 == "a" or ans1 == "c":
+      print ("Your answer is...")
+      time.sleep(1)
+      print ("Incorrect! The correct answer is B) Family!")
+      
 
-elif ans1 == "b":
-  print ("Your answer is...")
-  time.sleep(1)
-  print ("Correct! Good job on getting the first question right!")
+    elif ans1 == "b":
+      print ("Your answer is...")
+      time.sleep(1)
+      print ("Correct! Good job on getting the first question right!")
 
-else:
-  print ("I'm sorry, thats not one of the answers. Please pick one of the three options.")
+    else:
+      print ("I'm sorry, thats not one of the answers. Please pick one of the three options.")
 
 
 
